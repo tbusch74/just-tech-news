2346493ef2993591aba31dfd45f19063b1988bd7
Tony <abusch@css.edu>
2021-11-29 23:22:09 -0600

    vote model added

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { User, Post, Vote } = require('../../models');
 
 // GET /api/users
 router.get('/', (req, res) => {
@@ -14,26 +14,38 @@ router.get('/', (req, res) => {
       });
   });
 
-// GET /api/users/1
-router.get('/:id', (req, res) => {
+  router.get('/:id', (req, res) => {
     User.findOne({
-      attributes: {exclude: ['password']},
+      attributes: { exclude: ['password'] },
       where: {
         id: req.params.id
-      }
-    })
-    .then(dbUserData => {
-        if (!dbUserData) {
-            res.status(404).json({ message: 'No user found with this id' });
-            return;
+      },
+      include: [
+        {
+          model: Post,
+          attributes: ['id', 'title', 'post_url', 'created_at']
+        },
+        {
+          model: Post,
+          attributes: ['title'],
+          through: Vote,
+          as: 'voted_posts'
         }
-    res.json(dbUserData);
+      ]
     })
-    .catch(err => {
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No user found with this id' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => {
         console.log(err);
         res.status(500).json(err);
-    });
-});
+      });
+  });
+  
 
 // POST /api/users
 router.post('/', (req, res) => {

8626522a4bc4cdee2d3211a860a314ea24708457
Tony <abusch@css.edu>
2021-11-29 23:56:43 -0600

    add comment model

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -26,6 +26,14 @@ router.get('/', (req, res) => {
           attributes: ['id', 'title', 'post_url', 'created_at']
         },
         {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'created_at'],
+          include: {
+            model: Post,
+            attributes: ['title']
+          }
+        },
+        {
           model: Post,
           attributes: ['title'],
           through: Vote,
